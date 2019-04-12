48328127a4ffa4a0dc08ba37cefbfd80b34f5a84
exvo <wyangdyq@outlook.com>
2019-04-12 16:21:42 +0800

    update

--- code/exvo-example/exvo-base/src/main/java/com/example/demo/main/Test01.java
+++ code/exvo-example/exvo-base/src/main/java/com/example/demo/main/Test01.java
@@ -1,5 +1,14 @@
 package com.example.demo.main;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @Date: 2019/4/2
  * @Author: wu yang
@@ -49,12 +58,42 @@ public class Test01 {
 //        String a = "好的出s口啊呀";
 //        System.out.println(a.contains("出口"));
 
-        String str = "123|erere|454";
+        /*String str = "123|erere|454";
         String[] split = str.split("\\|");
         for (int i = 0; i < split.length; i++) {
             System.out.println(split[i]);
+        }*/
+
+        String[] a = {"a", "a", "a", "a"};
+        String[] b = {"b", "b", "b", "b"};
+        String[] c = {"c", "c", "c", "c"};
+        String[] d = {"d", "d", "d", "d"};
+
+        List<String[]> list1 = new ArrayList<>();
+        list1.add(b);
+        list1.add(c);
+
+        List<String> res = new ArrayList<>();
+        res.addAll(Arrays.asList(a));
+        res.addAll(Arrays.asList(b));
+        res.addAll(Arrays.asList(d));
+        list1.forEach(l -> {
+            for (int i = 0; i < l.length; i++) {
+                res.add(l[i]);
+            }
+        });
+
+//        res.forEach(r -> System.out.println(r));
+//        String[] dd = res.stream().toArray(String[]::new);
+//        System.out.println(dd);
+
+        String str = "nihao%d（wrrew）";
+        if (str.contains("%d")) {
+            str = String.format(str, 2);
         }
+        System.out.println(str);
 
     }
+
 
 }
---
+++ code/exvo-example/exvo-base/src/main/java/com/example/demo/main/Test02.java
@@ -0,0 +1,14 @@
+package com.example.demo.main;
+
+/**
+ * @Date: 2019/4/12
+ * @Author: wu yang
+ * @Description:
+ */
+public class Test02 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
